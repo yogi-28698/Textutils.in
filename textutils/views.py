6227d3f2e8f0c14a2c3a8f895b899bdf0b2d7f42
Yogesh Verma <64734013+yogi-28698@users.noreply.github.com>
2020-06-11 11:29:07 -0700

    Update views.py

--- textutils/views.py
+++ textutils/views.py
@@ -1,4 +1,3 @@
-# I Have Created This Website
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -12,18 +11,19 @@ def contact(request):
 
 def analyze(request):
 
-    # Getting The Text
- # if request.method == "post":
+    
+  if request.method == "post":
+    
     djtext = request.POST.get('text','default')
 
-    # Checking the checkbox
+    
     removepunc = request.POST.get('removepunc', 'off')
     capitalize = request.POST.get('capitalize', 'off')
     removespace = request.POST.get('removespace', 'off')
     removeline = request.POST.get('removeline', 'off')
     countchar = request.POST.get('charcount', 'off')
     fullcaps = request.POST.get('fullcaps', 'off')
-    # string = ""
+    
     punctuations = '''!()-[];:'"\,<>./?@#$%^&*_~{}'''
     analyzed = ""
 
@@ -45,13 +45,10 @@ def analyze(request):
         djtext = analyzed
     if removeline == 'on':
         analyzed = ""
-        # analyzed = djtext.replace('\n','').replace('\r','')
+        
         for char in djtext:
             if char != "\n" and char!="\r":
                 analyzed = analyzed + char
-            else:
-                print("no")
-        print("pre", analyzed)
         djtext = analyzed
 
     if fullcaps == 'on':
@@ -69,20 +66,3 @@ def analyze(request):
             analyzed += "Please Select any Operation and try again !"
     params = {'purpose': '', 'analyzed_text': analyzed}
     return render(request, 'analyze.html', params)
-    # return HttpResponse(s)
-#
-# def capfirst(request):
-#     s = '''<a href = "/">Back to main</a>'''
-#     return HttpResponse(s)
-#
-# def newlineremove(request):
-#     s = '''<a href = "/">Back to main</a>'''
-#     return HttpResponse(s)
-#
-# def spaceremove(request):
-#     s = '''<a href = "/">Back to main</a>'''
-#     return HttpResponse(s)
-#
-# def charcount(request):
-#     s = '''<a href = "/">Back to main</a>'''
-#     return HttpResponse(s)
